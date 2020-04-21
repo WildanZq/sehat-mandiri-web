b9dda9c7b105fd5212fe19c89f9861922a02e2fe
Wildan Ziaulhaq <wildanzhq@gmail.com>
2020-04-21 09:40:27 +0700

    Initial config

---
+++ application/views/v_login.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sehat Mandiri</title>
+</head>
+<body>
+    login
+</body>
+</html>
