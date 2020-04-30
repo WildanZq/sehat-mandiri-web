11f76e29bff76b0efce35dbb1580457346c79380
Wildan Ziaulhaq <wildanzhq@gmail.com>
2020-04-30 15:47:51 +0700

    Updating icon

--- application/views/pasien/registrasi_view.php
+++ application/views/pasien/registrasi_view.php
@@ -7,6 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sehat Mandiri</title>
+    <link rel="shortcut icon" href="<?php echo base_url('assets/icon/icon.png'); ?>" type="image/x-icon">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
 </head>
